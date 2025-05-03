0c5a52fb9d39a90ffa926b1a3666d30d1fa95c76
Stanislav <stanislavigov4@gamil.com>
2025-05-03 15:56:07 +0000

    home page and layout

--- client/src/pages/home/layout.tsx
+++ client/src/pages/home/layout.tsx
@@ -1,4 +1,4 @@
-const Home = () => {
+const HomeLayout = () => {
     return(
         <div className="home">
             <h1>HomePage</h1>
@@ -6,4 +6,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default HomeLayout;
