a903f008b4579cac37a0ccd6b863d962ab8ad4cb
Stanislav <stanislavigov4@gamil.com>
2025-05-04 09:35:19 +0000

    router moved from main to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ContactPage from './pages/contact'
-import HomePage from './pages/home'
-import LoginPage from './pages/login'
-import RegisterPage from './pages/register'
-import AboutPage from './pages/about'
-import ProductPage from './pages/product'
+import App from './App.tsx'
 
-const router = createBrowserRouter([
-  {path:"/", element:<HomePage/>},
-  {path:"/contact", element:<ContactPage/>},
-  {path:"/login", element:<LoginPage/>},
-  {path:"/register", element:<RegisterPage/>},
-  {path:"/about", element: <AboutPage/>},
-  {path:"/products", element: <ProductPage/>}
-])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router = {router}/>
+    {/* <RouterProvider router = {router}/> */}
+    <App/>
   </StrictMode>,
 )
