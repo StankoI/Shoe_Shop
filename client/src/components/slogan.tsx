6d86d95a6db7f1f375a66ee27b4da43b0ab9f869
Stanislav <stanislavigov4@gamil.com>
2025-05-12 10:49:23 +0000

    slogan component finished

--- client/src/components/slogan.tsx
+++ client/src/components/slogan.tsx
@@ -1,45 +1,45 @@
-import "./slogan.css";
+import styles from "./slogan.module.css";
 
 const Slogan = () => {
   return (
-    <div className="slogan-section">
-      <div className="slogan-text">
-        <div className="slogan-badge">
-          <div className="badge-new">New</div>
-          <div className="badge-label">Sustainable Collection 2025</div>
+    <div className={styles["slogan-section"]}>
+      <div className={styles["slogan-text"]}>
+        <div className={styles["slogan-badge"]}>
+          <div className={styles["badge-new"]}>New</div>
+          <div className={styles["badge-label"]}>Sustainable Collection 2025</div>
         </div>
 
-        <div className="slogan-heading">
+        <div className={styles["slogan-heading"]}>
           <div>Step</div>
           <div>Into</div>
-          <div className="text-green">Comfort</div>
+          <div className={styles["text-green"]}>Comfort</div>
           <div>, </div>
           <div>Walk</div>
-          <div>With</div> 
-          <div className="text-orange">Style </div>
+          <div>With</div>
+          <div className={styles["text-orange"]}>Style </div>
         </div>
 
-        <div className="slogan-description">
+        <div className={styles["slogan-description"]}>
           Discover our premium collection of shoes designed for comfort, style, and sustainability.
         </div>
 
-        <div className="slogan-buttons">
-          <button className="btn btn-black">Shop Now</button>
-          <button className="btn btn-white">Learn More</button>
+        <div className={styles["slogan-buttons"]}>
+          <button className={styles["btn"] + " " + styles["btn-black"]}>Shop Now</button>
+          <button className={styles["btn"] + " " + styles["btn-white"]}>Learn More</button>
         </div>
       </div>
 
-      <div className="slogan-image-wrapper">
-        <div className="slogan-image"></div>
-        <div className="color-options">
-          <div className="color-circle green"></div>
-          <div className="color-circle blue"></div>
-          <div className="color-circle orange"></div>
-          <div className="color-text">+12 Colors</div>
+      <div className={styles["slogan-image-wrapper"]}>
+        <div className={styles["slogan-image"]}></div>
+        <div className={styles["color-options"]}>
+          <div className={styles["color-circle"] + " " + styles["green"]}></div>
+          <div className={styles["color-circle"] + " " + styles["blue"]}></div>
+          <div className={styles["color-circle"] + " " + styles["orange"]}></div>
+          <div className={styles["color-text"]}>+12 Colors</div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Slogan;
