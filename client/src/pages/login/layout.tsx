79526f754a7d64d37cbd2bf9c1bc9fbdb2fad567
Stanislav <stanislavigov4@gmail.com>
2025-05-14 16:09:52 +0300

    finished login component

--- client/src/pages/login/layout.tsx
+++ client/src/pages/login/layout.tsx
@@ -1,7 +1,10 @@
+import LoginComponent from "../../components/layouts/login/loginComponent";
+
 const LoginLayout = () => {
     return(
         <div className="loginPage">
             <h1>Login page</h1>
+            <LoginComponent/>
         </div>
     );
 }
