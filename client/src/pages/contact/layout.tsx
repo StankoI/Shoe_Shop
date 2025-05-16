a2023008cc6eac0dab8dcc24b4d61fbe2f44de0f
Stanislav <stanislavigov4@gamil.com>
2025-05-16 09:41:39 +0000

    text component

--- client/src/pages/contact/layout.tsx
+++ client/src/pages/contact/layout.tsx
@@ -1,7 +1,10 @@
+import ContactTextComponent from "../../components/layouts/contactTextComponent/contactTextComponent";
+import styles from "./contact.module.css"
+
 const ContactLayout = () =>{
     return(
-        <div className="contactPage">
-            <h1>Contact Page</h1>
+        <div className= {styles["contactPage"]}>
+            <ContactTextComponent/>
         </div>
     );
 }
