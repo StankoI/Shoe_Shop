b1681462173e0ed8fcdcf23c48c7d0d400c2652e
Stanislav <stanislavigov4@gamil.com>
2025-05-19 21:33:03 +0000

    about page - about us component

--- client/src/pages/about/layout.tsx
+++ client/src/pages/about/layout.tsx
@@ -1,5 +1,3 @@
-import AboutUs from "../../components/layouts/aboutPageAboutUsComponent/aboutPageAboutUsComponent";
-import AboutPagePhotosComponent from "../../components/layouts/aboutPagePhotosComponent/aboutPagePhotosComponent";
 import AboutUsAndPhotoMerge from "../../components/layouts/aboutUsAndPhotoMerge/aboutUsAndPhotoMerge";
 import styles from "./about.module.css"
 
