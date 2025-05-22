d36bd137e06e1f7c576753dd708f9e9275ff1c00
Stanislav <stanislavigov4@gamil.com>
2025-05-22 18:05:52 +0000

    ratings model

--- server/src/models/product.js
+++ server/src/models/product.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
-    availability: { type: Number, default: 0 },
-    size: Number,
-    img: String,
-    categories: [String]
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type:String
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    availability: {
+        type: Number,
+        default: 0
+    },
+    size: {
+        type:Number
+    },
+    img: {
+        type:String
+    },
+    categories: 
+    {
+        type: [String]
+    },
+    ratingsIds: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ratings'
+    }]
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 }
