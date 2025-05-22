0d10e17fe64a9e41e17e0bb64c7597ea00196ce2
Stanislav <stanislavigov4@gamil.com>
2025-05-22 15:06:22 +0000

    added put delete get , getById

--- server/src/controllers/product.controler.js
+++ server/src/controllers/product.controler.js
@@ -1,6 +1,7 @@
+const product = require('../models/product');
 const Product = require('../models/product');
 
-async function addProduct (req, res){
+async function addProduct(req, res) {
     try {
         const {
             name,
@@ -30,5 +31,83 @@ async function addProduct (req, res){
     }
 }
 
+async function getAllProducts(req, res) {
+    try {
+        const products = await Product.find();
+        res.status(201).json(products);
+    }
+    catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
 
-module.exports = { addProduct };
+async function getProduct(req, res) {
+    try {
+        const id = req.params.id;
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "product not find" })
+        }
+
+        res.status(201).json(product);
+    }
+    catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
+
+async function deleteProduct(req, res) {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "product not find" })
+        }
+
+        res.status(201).json(product);
+    }
+    catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
+
+async function updateProduct(req, res) {
+    try {
+        const { id } = req.params;
+        const {
+            name,
+            description,
+            price,
+            availability,
+            size,
+            img,
+            categories
+        } = req.body;
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            id,
+            {
+                name,
+                description,
+                price,
+                availability,
+                size,
+                img,
+                categories
+            },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Продуктът не е намерен.' });
+        }
+
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+module.exports = { addProduct, getAllProducts, getProduct, deleteProduct, updateProduct};
--- server/src/routes/admin/product.router.js
+++ server/src/routes/admin/product.router.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct } = require('../../controllers/product.controler');
+const { addProduct, getAllProducts, getProduct, deleteProduct, updateProduct} = require('../../controllers/product.controler');
 
 router.post('/', addProduct);
+
+router.get('/', getAllProducts);
+
+router.get('/:id', getProduct);
+
+router.delete('/:id', deleteProduct);
+
+router.put('/:id', updateProduct);
 
 module.exports = router;
