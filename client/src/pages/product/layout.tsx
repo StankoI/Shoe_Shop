89b26a50b6bae64ea11cdc0dd59c67c2d399b448
Stanislav <stanislavigov4@gamil.com>
2025-05-24 12:52:28 +0000

    working on product list

--- client/src/pages/product/layout.tsx
+++ client/src/pages/product/layout.tsx
@@ -1,6 +1,7 @@
-import ProductItem from "../../components/product/productItem";
+import { useEffect, useState } from "react";
+import ProductList from "../../components/productList/productList";
 import Product from "../../models/product";
-
+import axios from 'axios'
 
 //     id: IdType;
 //     name: string;
@@ -13,7 +14,7 @@ import Product from "../../models/product";
 //     createdAt: Date;
 
 
-const prd: Product = {
+const prds: Product[] = [{
     id: "1",
     name: "Nike Air Max 270",
     description: "Леки и удобни маратонки за ежедневна употреба.",
@@ -21,14 +22,94 @@ const prd: Product = {
     img: "../../../public/placeholder.svg",
     categories: ["мъжки", "обувки", "спортни"],
     color: "black",
-    in_stock:[{size:43, quantity: 4},{size:44, quantity: 5},{size:45, quantity: 6}],
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "2",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "3",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "2",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "3",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "2",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
+    createdAt: new Date(Date.now()),
+},
+{
+    id: "3",
+    name: "Nike Air Max 270",
+    description: "Леки и удобни маратонки за ежедневна употреба.",
+    price: 199.99,
+    img: "../../../public/placeholder.svg",
+    categories: ["мъжки", "обувки", "спортни"],
+    color: "black",
+    in_stock: [{ size: 43, quantity: 4 }, { size: 44, quantity: 5 }, { size: 45, quantity: 6 }],
     createdAt: new Date(Date.now()),
 }
+]
 
 const ProductsLayout = () => {
+
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        
+        axios.get('http://localhost:8080/client/products')
+        .then(res => setProducts(res.data))
+        .catch(err => 
+            console.error(err)
+        )
+
+    })
+
     return (
-        <div className="productPage" style={{marginTop:"4em"}}>
-            <ProductItem product={ prd} />
+        <div className="productPage" style={{ marginTop: "4em" }}>
+            <ProductList products={products} />
         </div>
     );
 }
